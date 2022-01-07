e4b1829841dd45d834957ad947b17ea45750fb08
Erlend Hamnaberg <erlend@hamnaberg.net>
2022-01-07 14:54:19 +0100

    Slightly better detection of java

--- bleep/src/main/scala/bleep/JavaCmd.scala
+++ bleep/src/main/scala/bleep/JavaCmd.scala
@@ -1,6 +1,10 @@
 package bleep
 
-object JavaCmd {
-  val javacommand = if (sys.props("os.name").toLowerCase.contains("win")) "java" else "/usr/bin/java"
+import java.nio.file.{Files, Paths}
 
+object JavaCmd {
+  val defaultCommand = if (sys.props("os.name").toLowerCase.contains("win")) "java" else "/usr/bin/java"
+  val detected =
+    sys.env.get("JAVA_HOME").map(Paths.get(_)).map(_.resolve("bin").resolve("java")).filter(Files.exists(_)).map(_.toString)
+  val javacommand = detected.getOrElse(defaultCommand)
 }
